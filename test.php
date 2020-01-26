3f6c21881f64e3561784a7fe660e63eab8d39b8f
Arthur <nullbool@hotmail.com>
2020-01-26 21:46:30 +0200

    Add test page for temp dev actions

---
+++ test.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Test page");
+?>
+<!--Here is my test code area-->
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
