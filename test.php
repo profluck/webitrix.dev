9266e954e705cbdf9fd0dc0fb2f1ddc4d08ae09b
= <=>
2020-01-27 18:02:07 +0200

    Remove comment

--- test.php
+++ test.php
@@ -2,7 +2,6 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Test page");
 ?>
-<!--Here is my test code area-->
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
